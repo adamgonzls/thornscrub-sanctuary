df8e7623bd27baee981bb40a0cacfe3fb1c72c7a
Adam Gonzales <adamgonzales1@gmail.com>
2025-05-24 10:09:34 -0600

    content: update gallery content

---
+++ src/data/vars.ts
@@ -0,0 +1,30 @@
+import CollaredLizard from "src/images/lizard-smiling-373w.png"
+import Peyote from "src/images/peyote-lophophora-williamsii.png"
+import PencilCactus from "src/images/pencil-cactus.png"
+import Sunset from "src/images/sunset-1000w.png"
+
+export const scenesContent = [
+  {
+    title: "Reticulated Collared Lizard (Crotaphytus reticulatus)",
+    description:
+      "This lizard is native to South Texas and northeastern Mexico, known for its intricate net-like pattern and bright breeding colors. It thrives in the brushy, semiarid habitats of the Tamaulipan thornscrub",
+    imageUrl: CollaredLizard,
+  },
+  {
+    title: "Peyote (Lophophora williamsii)",
+    description:
+      "A small, spineless cactus with soft, button-like tops, sacred to many Indigenous cultures. It grows low in the rocky soils of South Texas and northern Mexico.",
+    imageUrl: Peyote,
+  },
+  {
+    title: "Echinocereus poselgeri (Pencil Cactus)",
+    description:
+      "A slender, sprawling cactus with narrow stems that resemble green pencils. It blooms with striking magenta flowers in spring and thrives in sandy, coastal scrublands",
+    imageUrl: PencilCactus,
+  },
+  {
+    title: "",
+    description: "Sunset at Thornscrub.",
+    imageUrl: Sunset,
+  },
+]
